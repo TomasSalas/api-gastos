66318de3b1e050a433095dc3be8f88517df3d231
Tomas Salas A <tomas.salas@amicar.cl>
2025-05-10 23:56:20 -0400

    fix url front deleted /

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://app-gastos-production.up.railway.app/'],
+  origin: ['http://localhost:5173', 'https://app-gastos-production.up.railway.app'],
   credentials: true
 }))
 
