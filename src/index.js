8862e5d918924537860e9e18d554673011bec03b
Tomas Salas A <tomas.salas@amicar.cl>
2025-05-10 23:44:07 -0400

    add cors new

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://app-gastos.railway.internal'],
   credentials: true
 }))
 
